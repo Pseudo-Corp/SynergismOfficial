0de1ca01120ce9249f05f9719e6cb486ef6329f6
AlienC4 <20047400+AlienC4@users.noreply.github.com>
2020-09-20 20:04:03 +0300

    Change the offering buy text to match the new functionality

--- Javascript/runes.js
+++ Javascript/runes.js
@@ -10,9 +10,6 @@ function displayRuneInformation(i, updatelevelup) {
     let m = effectiveLevelMult
     let SILevelMult = (1 + player.researches[84] / 200)
     let amountPerOffering = calculateRuneExpGiven(i - 1, false, player.runelevels[i - 1]);
-    if (player.upgrades[78] === 1) {
-        document.getElementById("toggleofferingbuy").textContent = "Toggle amount used by sacrifice, multiplied by 1000 due to a Reincarnation Upgrade.";
-    }
 
 
     if (i === 1) {
