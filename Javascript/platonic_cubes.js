8e5dbdcdfd1c96797a697bf88616f1ab87365524
AlienC4 <20047400+AlienC4@users.noreply.github.com>
2020-11-14 02:48:41 +0200

    Formatting

--- Javascript/platonic_cubes.js
+++ Javascript/platonic_cubes.js
@@ -12,14 +12,14 @@ function openPlatonic(value, max) {
     let toSpendDiv40000 = Math.floor(toSpend / 40000)
     console.log(toSpendDiv40000)
     let blessings = {
-        cubes:   {weight: 13200, pdf: (x) => 0 <= x && x <= 33.000},
-        tesseracts:    {weight: 13200, pdf: (x) => 33.000 < x && x <= 66.000},
-        hypercubes:      {weight: 13200, pdf: (x) => 66.000 < x && x <= 99.000},
-        platonics:       {weight: 396, pdf: (x) => 99.000 < x && x <= 99.990},
-        hypercubeBonus:     {weight: 1, pdf: (x) => 99.990 < x && x <= 99.9925},
-        taxes:      {weight: 1, pdf: (x) => 99.9925 < x && x <= 99.995},
-        scoreBonus:  {weight: 1, pdf: (x) => 99.995 < x && x <= 99.9975},
-        globalSpeed:        {weight: 1, pdf: (x) => 99.9975 < x && x <= 100},
+        cubes:          {weight: 13200, pdf: (x) => 0 <= x && x <= 33.000},
+        tesseracts:     {weight: 13200, pdf: (x) => 33.000 < x && x <= 66.000},
+        hypercubes:     {weight: 13200, pdf: (x) => 66.000 < x && x <= 99.000},
+        platonics:      {weight: 396, pdf: (x) => 99.000 < x && x <= 99.990},
+        hypercubeBonus: {weight: 1, pdf: (x) => 99.990 < x && x <= 99.9925},
+        taxes:          {weight: 1, pdf: (x) => 99.9925 < x && x <= 99.995},
+        scoreBonus:     {weight: 1, pdf: (x) => 99.995 < x && x <= 99.9975},
+        globalSpeed:    {weight: 1, pdf: (x) => 99.9975 < x && x <= 100},
     }
 
     //If you're opening more than 40,000 Platonics, it will consume all Platonics until remainder mod 40,000, giving expected values.
@@ -28,13 +28,16 @@ function openPlatonic(value, max) {
     }
     //Then, the remaining hypercube will be opened, simulating the probability [RNG Element]
     let RNGesus = ['hypercubeBonus', 'taxes', 'scoreBonus', 'globalSpeed']
-    for(var i = 0; i < RNGesus.length; i++){
+    for (let i = 0; i < RNGesus.length; i++) {
         let num = Math.random();
-        if(toSpendModulo/40000 >= num && toSpendModulo !== 0){player.platonicBlessings[RNGesus[i]] += 1; toSpendModulo -= 1}
+        if (toSpendModulo / 40000 >= num && toSpendModulo !== 0) {
+            player.platonicBlessings[RNGesus[i]] += 1;
+            toSpendModulo -= 1
+        }
     }
-    let gainValues = [Math.floor(33*toSpendModulo/100), Math.floor(33*toSpendModulo/100), Math.floor(33*toSpendModulo/100), Math.floor(396*toSpendModulo/40000)]
-    let commonDrops = ['cubes','tesseracts','hypercubes','platonics']
-    for(var i = 0; i < commonDrops.length; i++){
+    let gainValues = [Math.floor(33 * toSpendModulo / 100), Math.floor(33 * toSpendModulo / 100), Math.floor(33 * toSpendModulo / 100), Math.floor(396 * toSpendModulo / 40000)]
+    let commonDrops = ['cubes', 'tesseracts', 'hypercubes', 'platonics']
+    for (let i = 0; i < commonDrops.length; i++) {
         player.platonicBlessings[commonDrops[i]] += gainValues[i]
         toSpendModulo -= gainValues[i]
     }
