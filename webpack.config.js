9ee1e9e2fcadd7ee9dd46fe353dca558e9ac0050
Andreas Tschritter <a.tschritter@kapilendo.de>
2021-01-30 23:20:32 +0100

    Enable hot reloading and source maps

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,16 @@
 const path = require('path');
 
-module.exports = {
+module.exports = (env, argv) => ({
     entry: './src/Synergism.ts',
-    mode: 'none',
+    mode: argv.mode,
+    devtool: argv.mode === 'development' ? 'eval-source-map' : 'source-map',
     module: {
         rules: [{
             test: /\.tsx?$/,
             use: 'ts-loader',
             exclude: /node_modules/,
         }],
+        
     },
     resolve: {
         extensions: [ '.ts', '.js' ],
@@ -17,8 +19,13 @@ module.exports = {
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist'),
+        publicPath: '/dist/'
     },
     devServer: {
+        stats: {
+            children: false, // Hide children information
+            maxModules: 0 // Set the maximum number of modules to be shown
+        },
         port: 3001 // Specify a port number to listen for requests
     },
-};
+});
