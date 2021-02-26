6ae1136c9886f82b8a3c02156a8d66e594e8ab47
Khafra <42794878+KhafraDev@users.noreply.github.com>
2021-02-25 19:43:03 -0500

    so legacy files had both of these keys at some point

--- src/types/LegacySynergism.d.ts
+++ src/types/LegacySynergism.d.ts
@@ -14,7 +14,10 @@ export interface LegacyShopUpgrades {
     talismanBought: boolean
     challengeExtension: number
     challenge10Tomes: number
+    // in v1.0101 this is correct
     seasonPass: number
+    // in v2.1.2 this is correct
+    seasonPassLevel: number
     cubeToQuarkBought: boolean
     tesseractToQuarkBought: boolean
     hypercubeToQuarkBought: boolean
