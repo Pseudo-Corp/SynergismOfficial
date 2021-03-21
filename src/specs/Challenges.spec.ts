fc3a7a24e28d6b9cd8fde7038635f8fa36f68676
Khafra <42794878+KhafraDev@users.noreply.github.com>
2021-03-21 00:41:42 -0400

    fix build error

--- src/specs/Challenges.spec.ts
+++
@@ -1,6 +0,0 @@
-import { getMaxChallenges, challengeDisplay, getChallengeConditions,
-    toggleRetryChallenges, highestChallengeRewards, calculateChallengeRequirementMultiplier,
-    CalcECC, challengeRequirement, runChallengeSweep } from "../Challenges";
-
-/* Implement tests as needed below!
-    I'll add tests later, I think some setup is needed -Plat */
