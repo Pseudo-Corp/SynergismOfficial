261b9d26621772e9c11ccdb6438fbb19f3ab7b91
Khafra <42794878+KhafraDev@users.noreply.github.com>
2021-04-01 15:07:03 -0400

    add custom amount to base class

--- src/CubeExperimental.ts
+++ src/CubeExperimental.ts
@@ -9,10 +9,29 @@ want, though!
 Thank you! */
 
 import Decimal, { DecimalSource } from 'break_infinity.js';
+import { player } from './Synergism';
+import { Prompt, Alert } from './UpdateHTML';
 
+/**
+ * @description Generic class for handling cube subsets.
+ * @example
+ * class PlatCubes extends Currency {
+ *   constructor() {
+ *       super('wowPlatonicCubes', player.wowPlatonicCubes);
+ *   }
+ *
+ *   async open(amount: number, value: boolean) {
+ *       // implement open logic here
+ *   }
+ * }
+ * 
+ * new PlatCubes().openCustom(); 
+ */
 export abstract class Currency extends Decimal {
+    /** key on the player object */
     type: string;
-    constructor(
+
+    constructor (
         type: string,
         v?: DecimalSource
     ) {
@@ -20,9 +39,27 @@ export abstract class Currency extends Decimal {
         this.type = type;
     }
 
-    /** Open a set amount */
+    /**
+     * @description Open a given amount of cubes
+     * @param amount Number of cubes to open
+     * @param max if true, overwrites amount and opens the max amount of cubes.
+     */
     abstract open(amount: number, max: boolean): Promise<void>;
 
-    /** Open a custom amount */
-    abstract openCustom(): Promise<void>;
+    /** Open a custom amount of cubes */
+    async openCustom() {
+        const amount = await Prompt(`How many cubes would you like to open? You have ${player[this.type].toLocaleString()}!`);
+        if (amount === null)
+            return Alert('OK. No cubes opened.');
+        const cubesToOpen = Number(amount);
+
+        if (Number.isNaN(cubesToOpen) || !Number.isFinite(cubesToOpen)) // nan + Infinity checks
+            return Alert('Value must be a finite number!');
+        else if (player[this.type] < cubesToOpen) // not enough cubes to open
+            return Alert('You don\'t have enough cubes to open!');
+        else if (cubesToOpen <= 0) // 0 or less cubes to open
+            return Alert('You can\'t open a negative number of cubes.');
+
+        return this.open(cubesToOpen, cubesToOpen === player[this.type]);
+    }
 }
