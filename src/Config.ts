60df3ea49a2b01ebb8da3551de6eb259ba376640
Cephyric <tom@tennoc.com>
2021-07-10 23:00:24 +0100

    Define an encoding type of readFileSync to avoid having to convert to/from a buffer object, use regex test rather than match, remove inferred primitive

--- src/Config.ts
+++ src/Config.ts
@@ -1,3 +1,3 @@
 export const version = '2.5.5';
 export const testing = false;
-export const lastUpdated: Date = new Date('##LAST_UPDATED##');
+export const lastUpdated = new Date('##LAST_UPDATED##');
--- update-last-updated.js
+++ update-last-updated.js
@@ -7,20 +7,20 @@ console.log('\x1b[33m%s\x1b[0m', `Updating last updated config date to ${UTC}`);
 
 try {
     console.log('\x1b[33m%s\x1b[0m', 'Reading file & updating ##LAST_UPDATED##')
-    let config = readFileSync(`${__dirname}/src/Config.ts`).toString();
+    let config = readFileSync(`${__dirname}/src/Config.ts`, 'utf-8');
 
-    if (!config.match(/'##LAST_UPDATED##'/g)) {
+    if (!/'##LAST_UPDATED##'/g.test(config)) {
         throw new Error('File does not contain the \'##LAST_UPDATED##\' placeholder');
     }
 
     config = config.replace(/'##LAST_UPDATED##'/g, UTC);
 
-    if (!config.match(/Date\.UTC\(/g)) {
+    if (!/Date\.UTC\(/g.test(config)) {
         throw new Error('Couldn\'t replace the placeholder');
     }
 
     console.log('\x1b[33m%s\x1b[0m', 'writing file back to source')
-    writeFileSync(`${__dirname}/src/Config.ts`, Buffer.from(config));
+    writeFileSync(`${__dirname}/src/Config.ts`, config);
     console.log('\x1b[32m%s\x1b[0m', 'Date updated');
 } catch (e) {
     console.error(e);
