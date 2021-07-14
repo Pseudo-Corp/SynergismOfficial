f769bc06a68605c4c2f2c9854524c7a18b26959b
strentax <strentax@gmail.com>
2021-07-14 15:51:20 -0400

    Update Corruptions.ts
    Fix array references

--- src/Corruptions.ts
+++ src/Corruptions.ts
@@ -240,7 +240,7 @@ async function corruptionLoadoutGetNewName(loadout = 1) {
     const maxChars = 9
     const regex = /^[\x00-\xFF]*$/
     const renamePrompt = await Prompt(
-        `What would you like to name Loadout ${loadout}? ` +
+        `What would you like to name Loadout ${loadout - 1}? ` +
         `Names cannot be longer than ${maxChars} characters. Nothing crazy!`
     );
    
@@ -254,7 +254,7 @@ async function corruptionLoadoutGetNewName(loadout = 1) {
         return Alert("The Loadout Renamer didn't like a character in your name! Try something else.")
     }
     else {
-        player.corruptionLoadoutNames[loadout] = renamePrompt
+        player.corruptionLoadoutNames[loadout - 1] = renamePrompt
         updateCorruptionLoadoutNames();
     }
 }
@@ -264,10 +264,10 @@ export const updateCorruptionLoadoutNames = () => {
     for (let i = 1; i < Object.keys(player.corruptionLoadouts).length + 1; i++) {
         const cells = rows[i + 1].cells
         if (cells[0].textContent.length === 0) {  //first time setup
-            cells[0].addEventListener('click', () => corruptionLoadoutGetNewName(i));
+            cells[0].addEventListener('click', () => corruptionLoadoutGetNewName(i - 1));
             cells[0].classList.add('corrLoadoutName');
         }
-        cells[0].textContent = `${player.corruptionLoadoutNames[i]}:`;
+        cells[0].textContent = `${player.corruptionLoadoutNames[i - 1]}:`;
     }    
 }
 
