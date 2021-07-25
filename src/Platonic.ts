3e7b5b18474404d75cc71418557e5917b250049b
AreYouKittenMe <87166512+AreYouKittenMe@users.noreply.github.com>
2021-07-25 17:28:58 +0200

    Update Platonic.ts

--- src/Platonic.ts
+++ src/Platonic.ts
@@ -254,15 +254,13 @@ const checkPlatonicUpgrade = (index: number): Record<keyof (IPlatBaseCost & { ca
         canBuy: false,
     }
 
-    //Let's do a proper loop over everything here instead of the appended extra check for Abyssals - more future-for Octeracts
-		
     let res_owned=0;
     for (let i = 0; i < resources.length; i++) {
-				if (resourceNames[i] === "wowAbyssals") {
-				    res_owned=player.hepteractCrafts.abyss.BAL
-				} else {
-						res_owned=player[resourceNames[i]]
-				}
+	if (resourceNames[i] === "wowAbyssals") {
+		res_owned=player.hepteractCrafts.abyss.BAL
+	} else {
+		res_owned=player[resourceNames[i]]
+	}
         if (platUpgradeBaseCosts[index][resources[i]] <= res_owned) {
             checksum++;
             checks[resources[i]] = true
