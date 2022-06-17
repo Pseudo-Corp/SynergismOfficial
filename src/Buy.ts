fad958d1666d4c29500114257a8e7558bf02d815
httpsnet <ituminakao@yahoo.co.jp>
2022-06-18 03:27:32 +0900

    oops

--- src/Buy.ts
+++ src/Buy.ts
@@ -1026,7 +1026,7 @@ export const buyAllBlessings = (type: 'Blessings' | 'Spirits', percentage = 100,
                 }
 
                 const [level, cost] = calculateSummationLinear(baseLevels, baseCost, runeshards, levelCap);
-                if (level > baseLevels && (!auto || (level - baseLevels) * 10000) > baseLevels) {
+                if (level > baseLevels && (!auto || (level - baseLevels) * 10000 > baseLevels)) {
                     if (type === 'Spirits') {
                         player.runeSpiritLevels[index] = level;
                     } else {
