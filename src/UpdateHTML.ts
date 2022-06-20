67bd0541e1620057a91b0d47cda82850fb7e819e
awWhy <107661324+awWhy@users.noreply.github.com>
2022-06-20 03:34:08 +0300

    Make Singularity not be affected by Ligth mode
    This is stupid, also allows to easily make it turn green if auto ever added

--- src/UpdateHTML.ts
+++ src/UpdateHTML.ts
@@ -613,6 +613,9 @@ export const buttoncolorchange = () => {
         DOMCacheGetOrSet('ascendbtn').style.backgroundColor = 'green' :
         DOMCacheGetOrSet('ascendbtn').style.backgroundColor = '#171717';
 
+    (player.singularityCount > 0.5)
+        DOMCacheGetOrSet('singularitybtn').style.backgroundColor = '#171717';
+
     if (G['currentTab'] === 'buildings' && G['buildingSubTab'] === 'coin') {
         const a = DOMCacheGetOrSet('buycoin1');
         const b = DOMCacheGetOrSet('buycoin2');
