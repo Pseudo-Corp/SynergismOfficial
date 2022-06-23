5acd0df2cce17cf6741954f66ee97e36a5a657da
ArvingoMaster <80004380+ArvingoMaster@users.noreply.github.com>
2022-06-23 12:06:41 -0400

    Fix conflicts and lint

--- src/HelpSettings.ts
+++ src/HelpSettings.ts
@@ -1,25 +1,5 @@
 import { DOMCacheGetOrSet } from './Cache/DOM';
-<<<<<<< Updated upstream
-const coinText = `
-Welcome to Synergism! Here is where you start of course. The start is very simple, use coins to buy buildings and 
-upgrades that produce even more coins! Keep doing it until you can do the next feature, you will know when you can see it. :)`
-const diamondText = `
-Congrats! You have prestiged! Prestiging for the first time unlocks the Diamond layer, which consists of Diamond buildings, 
-Diamond upgrades, automation upgrades (for your QoL needs), generator upgrades (powerful), and last but not least RUNES!
-Runes will be talked about in a different section.
-The diamond upgrades themselves are pretty decent, but a good amount of boost comes from the Diamond buildings! Once bought,
-the first tier will make crystals which will boost coin production and every tier after will produce the tier before.
-The final item unlocked is a new "prestige" layer, accelerator boosts! They will reset diamond upgrades and your diamonds (NOT
-GENERATOR UPGRADES, AUTOMATION, or DIAMOND BUILDINGS) in exchange for a boost to your accelerators. This can be useful at times but
-generally resetting more than 3-4 times is not necessary.
-`
-const associated = new Map<string, string>([
-    ['helpCoin', coinText],
-    ['helpDiamond', diamondText]
-]);
-=======
 import { player } from './Synergism';
->>>>>>> Stashed changes
 export const displayHelp = (btn: HTMLElement) => {
     const coinText = `
     Welcome to <span style='color:yellow'>Sy</span><span style='color:skyblue'>ne</span><span style='color:purple'>rg</span><span style='color:limegreen'>is</span><span style='color:orange'>m!</span> Here is where you start of course. The start is very simple, use coins to buy buildings and upgrades that produce even more coins! Keep doing it until you can do the next feature, you will know when you can see it. :)
@@ -87,7 +67,7 @@ export const displayHelp = (btn: HTMLElement) => {
     However, the further you get in challenge 15, the more <span style='color:lime'>bonuses</span> you unlock and the <span style='color:lime'>stronger</span> those bonuses get!
     You can check the bonuses in the <b>"Challenge 15/Platonic Upgrades"</b> subtab of "<span style='color:gold'>Wow! Cubes</span>".
     <span style=font-size:30px><i>STRATEGY:</i></span>
-    Think about this, at the beginning every<span style='color:lime'> reincarnation</span> done in c15 will grant you <b>1</b> obtainium, due to the <span style='color:red'>obtainium decrease \</span>from corruptions, so <i>is there any way to gain obtainium fast in c15?</i>
+    Think about this, at the beginning every<span style='color:lime'> reincarnation</span> done in c15 will grant you <b>1</b> obtainium, due to the <span style='color:red'>obtainium decrease </span>from corruptions, so <i>is there any way to gain obtainium fast in c15?</i>
     Remember, exiting and entering <span style='color:lime'>reincarnation challenges</span> does a <b>force-reincarnation</b> which grants you <b>1</b> obtanium. Maybe with the tools we have we can <span style='color:yellow'>enter</span> those challenges and <span style='color:yellow'>exit them very fast</span> to gain <span style='color:steelblue'> meaningful obtanium</span>? (<b>Timers set to 0 seconds will do them every frame!</b>)
     The main<span style='color:lime'> boost</span> in c15 is believe it or not<span style='color:skyblue'> crystals</span>, which means that <span style='color:skyblue'><b>PRISM</b></span>, the arguably worst rune, is one of the<span style='color:lime'> best</span> runes in c15, and that<span style='color:steelblue'> researches</span> that boost rune effectiveness or crystals are the only ones that are really gonna be able to do anything.
 
