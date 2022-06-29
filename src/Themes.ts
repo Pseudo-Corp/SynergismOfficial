a18c68fe288928b1a6699c37cf50c313eb41dfd1
awWhy <107661324+awWhy@users.noreply.github.com>
2022-06-29 15:33:04 +0300

    Themes Rework
    1 more theme and Light mode rework

--- src/Themes.ts
+++ src/Themes.ts
@@ -3,16 +3,20 @@ import { DOMCacheGetOrSet } from './Cache/DOM';
 export const toggleTheme = () => {
     const current = document.body.classList.contains('light')
         ? 'light'
-        : 'dark';
+        : 'dark'
+    const current2 = document.body.classList.contains('dark')
+        ? 'dark'
+        : 'lightDark'
 
     const themeButton = DOMCacheGetOrSet('theme');
 
-    if (current === 'dark') {
+    if (current === 'dark') { //Switches to 'Lighter Dark Mode'
         document.body.classList.remove('dark');
-        document.body.classList.add('light');
+        document.body.classList.add('lightDark');
         DOMCacheGetOrSet('singularitybtn').style.backgroundColor = '#171717';
         DOMCacheGetOrSet('rune6').style.backgroundColor = '#171717';
         DOMCacheGetOrSet('rune7').style.backgroundColor = '#171717';
+        DOMCacheGetOrSet('importFileButton').style.backgroundColor = '#0f0f0f'
         //It will remove Orange BG for next 5 runes
         DOMCacheGetOrSet('rune1').style.backgroundColor = '#171717';
         DOMCacheGetOrSet('rune2').style.backgroundColor = '#171717';
@@ -20,11 +24,30 @@ export const toggleTheme = () => {
         DOMCacheGetOrSet('rune4').style.backgroundColor = '#171717';
         DOMCacheGetOrSet('rune5').style.backgroundColor = '#171717';
 
-        themeButton.textContent = 'Dark Mode';
-    } else {
-        document.body.classList.remove('light');
-        document.body.classList.add('dark');
+        themeButton.textContent = 'Lighter Dark Mode';
+    }
+    if (current2 === 'lightDark') { //Switches to 'Light Mode'
+        document.body.classList.remove('lightDark');
+        document.body.classList.add('light');
+        DOMCacheGetOrSet('singularitybtn').style.backgroundColor = '#171717';
+        DOMCacheGetOrSet('rune6').style.backgroundColor = '#171717';
+        DOMCacheGetOrSet('rune7').style.backgroundColor = '#171717';
+        DOMCacheGetOrSet('importFileButton').style.backgroundColor = '#000000'
+        DOMCacheGetOrSet('achievementcolorcode2').style.color = '#ff01f3'
+        DOMCacheGetOrSet('rune1').style.backgroundColor = '#171717';
+        DOMCacheGetOrSet('rune2').style.backgroundColor = '#171717';
+        DOMCacheGetOrSet('rune3').style.backgroundColor = '#171717';
+        DOMCacheGetOrSet('rune4').style.backgroundColor = '#171717';
+        DOMCacheGetOrSet('rune5').style.backgroundColor = '#171717';
 
         themeButton.textContent = 'Light Mode';
+    }
+    if (current === 'light') { //Switches to 'Dark Mode'
+        document.body.classList.remove('light');
+        document.body.classList.add('dark');
+        DOMCacheGetOrSet('importFileButton').style.backgroundColor = '#171717'
+        DOMCacheGetOrSet('achievementcolorcode2').style.color = '#800080'
+
+        themeButton.textContent = 'Dark Mode';
     }
 }
