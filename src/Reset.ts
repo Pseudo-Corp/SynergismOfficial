78a47d02463c8f07451b0854ae34b9de9a16caa8
William Austin Byers <78285417+ab3327@users.noreply.github.com>
2022-08-09 12:01:12 -0400

    Preserve autoOrb state across sing

--- src/Reset.ts
+++ src/Reset.ts
@@ -935,6 +935,7 @@ export const singularity = async (): Promise<void> => {
     toggleSubTab(9, 0); // set 'corruption main'
     toggleSubTab(-1, 0); // set 'statistics main'
 
+    hold.overfluxOrbsAutoBuy = player.overfluxOrbsAutoBuy
     hold.totalQuarksEver = player.totalQuarksEver
     hold.singularityCount = player.singularityCount;
     hold.goldenQuarks = player.goldenQuarks;
