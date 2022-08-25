6747f296f56d0db113476f8365b140f140a66b8b
Pseudonian <Pseudonian@gmail.com>
2022-08-25 00:36:31 -0400

    REMOVE Testing

--- src/Config.ts
+++ src/Config.ts
@@ -1,4 +1,4 @@
 export const version = '2.9.11: August 24 Edition';
 // eslint-disable-next-line
-export const testing: boolean = true;
+export const testing: boolean = false;
 export const lastUpdated = new Date('##LAST_UPDATED##');
