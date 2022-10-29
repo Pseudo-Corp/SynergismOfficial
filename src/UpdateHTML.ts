5d1f2c6a1d3541b7c1053d8cbbbf350077ff5f2c
Khafra <42794878+KhafraDev@users.noreply.github.com>
2022-10-29 19:45:54 -0400

    Update src/UpdateHTML.ts

--- src/UpdateHTML.ts
+++ src/UpdateHTML.ts
@@ -862,7 +862,7 @@ export const updateChallengeLevel = (k: number) => {
     const maxChallenges = getMaxChallenges(k);
 
     if (k === 15) {
-        el.textContent = format(player.challenge15Exponent,0,true);
+        el.textContent = format(player.challenge15Exponent, 0, true);
     } else {
         el.textContent = `${player.challengecompletions[k]}/${maxChallenges}`;
     }
