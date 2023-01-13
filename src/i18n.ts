d782eb44c2db1836a51f5ec1c690ce7c8c649237
Khafra <42794878+KhafraDev@users.noreply.github.com>
2023-01-13 12:04:37 -0500

    fix: add i18n file

---
+++ src/i18n.ts
@@ -0,0 +1,21 @@
+import i18next, { type Resource } from 'i18next'
+import { prod } from './Config'
+
+const supported = ['en']
+
+export const init = async () => {
+    const resources: Record<string, Resource> = {}
+
+    for (const lang of supported) {
+        const response = await fetch(`./translations/${lang}.json`)
+        resources[lang] = {
+            translation: await response.json() as Resource
+        }
+    }
+
+    return i18next.init({
+        fallbackLng: 'en',
+        debug: !prod,
+        resources
+    })
+}
