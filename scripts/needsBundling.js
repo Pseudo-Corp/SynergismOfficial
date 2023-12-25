20618e0de182e0065749bf6d10b47831df818fd9
KhafraDev <maitken033380023@gmail.com>
2023-12-25 23:01:44 +0000

    Deploying to gh-pages from  @ 84aa009bd20731956581449bff25288ef95b4b5c 🚀

--- scripts/needsBundling.js
+++
@@ -1,9 +0,0 @@
-const { existsSync } = require('fs')
-
-if (existsSync('dist')) {
-  console.log('Skipping, build output already exists!')
-} else if (!existsSync('package.json')) {
-  console.log('Skipping, package.json does not exist -- dependencies cannot be installed')
-} else {
-  process.exit(1)
-}
