d4debb57ed6bed0d0dae657e8b0e67ef7b7bbd36
Kevin Bunn <45328239+Pseudonian@users.noreply.github.com>
2024-07-04 23:11:24 -0400

    Translations 2

--- src/Config.ts
+++ src/Config.ts
@@ -8,7 +8,7 @@ export const version = '3.0.0 pt 6: July 4, 2024: Pixel Update Beta [I]'
  * PSEUDO DO NOT CHANGE THIS LINE
  * PSEUDO DO NOT CHANGE THIS LINE
  */
-export const testing: boolean = false
+export const testing: boolean = true
 export const lastUpdated = new Date('##LAST_UPDATED##')
 /**
  * CHANGE THIS ONE INSTEAD
