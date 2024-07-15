86d9c0f7af38d6b9717415b8eee4dd2764f4a515
kimihiro64 <jonaswhidden@gmail.com>
2024-07-14 21:54:18 -0500

    Update Synergism.ts
    syntax

--- src/Synergism.ts
+++ src/Synergism.ts
@@ -1619,7 +1619,7 @@ const loadSynergy = async () => {
     // fix current sing challenge blank
     if (player.insideSingularityChallenge) {
       let challenges = Object.keys(player.singularityChallenges);
-      for (var i = 0; i < challenges.length(); i++) {
+      for (var i = 0; i < challenges.length; i++) {
         if (player.singularityChallenges[challenges[i]].enabled) {
           G.currentSingChallenge = challenges[i];
           break;
