dc20a7930ece482087ae0bd3070c6c741ad4e8cd
Khafra <maitken033380023@gmail.com>
2025-02-14 19:27:01 -0500

    mark as testing

--- src/Config.ts
+++ src/Config.ts
@@ -3,16 +3,9 @@ declare const PROD: boolean | undefined
 export const version = '3.1.2 February 1 2025: The Events Update'
 
 /**
- * PSEUDO DO NOT CHANGE THIS LINE
- * PSEUDO DO NOT CHANGE THIS LINE
- * PSEUDO DO NOT CHANGE THIS LINE
- * PSEUDO DO NOT CHANGE THIS LINE
- * PSEUDO DO NOT CHANGE THIS LINE
- * PSEUDO DO NOT CHANGE THIS LINE
+ * If true, the version is marked as a testing version.
  */
-export const testing: boolean = false
+export const testing = true
 export const lastUpdated = new Date('##LAST_UPDATED##')
-/**
- * CHANGE THIS ONE INSTEAD
- */
+
 export const prod = typeof PROD === 'undefined' ? false : PROD
