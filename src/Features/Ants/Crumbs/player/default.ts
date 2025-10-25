1d5ea6932005dae76a609a2abea5325fa1108c52
Kevin Bunn <45328239+Pseudonian@users.noreply.github.com>
2025-10-25 18:47:12 -0400

    Utilize default values

--- src/Features/Ants/Crumbs/player/default.ts
+++ src/Features/Ants/Crumbs/player/default.ts
@@ -1,5 +1,3 @@
-import Decimal from 'break_infinity.js'
-
-export const defaultCrumbs = Decimal.fromString('1')
-export const defaultHighestCrumbsThisSacrifice = Decimal.fromString('1')
-export const defaultHighestCrumbsEver = Decimal.fromString('1')
+export const defaultCrumbs = '1'
+export const defaultHighestCrumbsThisSacrifice = '1'
+export const defaultHighestCrumbsEver = '1'
