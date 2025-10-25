faedfa230cb6db43de7523b04082bf3a509feb31
Kevin Bunn <45328239+Pseudonian@users.noreply.github.com>
2025-10-25 17:45:00 -0400

    Port Ant Crumbs to Ant Feature

---
+++ src/Features/Ants/player/default.ts
@@ -0,0 +1,30 @@
+import Decimal from 'break_infinity.js'
+import { defaultAntMasteries } from '../AntMasteries/player/default'
+import { defaultAntProducers } from '../AntProducers/player/default'
+import { defaultAntSacrificeCount, defaultCurrentSacrificeId } from '../AntSacrifice/player/default'
+import { defaultAntImmortalELO } from '../AntSacrifice/Rewards/ELO/ImmortalELO/player/default'
+import {
+  defaultAntRebornELO,
+  defaultHighestRebornELODaily,
+  defaultHighestRebornELOEver,
+  defaultQuarksGainedFromAnts
+} from '../AntSacrifice/Rewards/ELO/RebornELO/player/default'
+import { defaultAntUpgrades } from '../AntUpgrades/player/default'
+import { defaultCrumbs, defaultHighestCrumbsEver, defaultHighestCrumbsThisSacrifice } from '../Crumbs/player/default'
+import type { PlayerAnts } from '../structs/structs'
+
+export const defaultPlayerAnts: PlayerAnts = {
+  producers: { ...defaultAntProducers },
+  masteries: { ...defaultAntMasteries },
+  upgrades: { ...defaultAntUpgrades },
+  crumbs: new Decimal(defaultCrumbs),
+  highestCrumbsThisSacrifice: new Decimal(defaultHighestCrumbsThisSacrifice),
+  highestCrumbsEver: new Decimal(defaultHighestCrumbsEver),
+  immortalELO: defaultAntImmortalELO,
+  rebornELO: defaultAntRebornELO,
+  highestRebornELODaily: [...defaultHighestRebornELODaily],
+  highestRebornELOEver: [...defaultHighestRebornELOEver],
+  quarksGainedFromAnts: defaultQuarksGainedFromAnts,
+  antSacrificeCount: defaultAntSacrificeCount,
+  currentSacrificeId: defaultCurrentSacrificeId
+}
