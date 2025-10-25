01791914fd117dbcaec5040f1cd578fab159574d
Khafra <maitken033380023@gmail.com>
2025-10-25 18:17:17 -0400

    show what I mean

--- src/Features/Ants/Crumbs/player/default.ts
+++ src/Features/Ants/Crumbs/player/default.ts
@@ -1,5 +1,5 @@
 import Decimal from 'break_infinity.js'
 
-export const defaultCrumbs = new Decimal('1')
-export const defaultHighestCrumbsThisSacrifice = new Decimal('1')
-export const defaultHighestCrumbsEver = new Decimal('1')
+export const defaultCrumbs = Decimal.fromString('1')
+export const defaultHighestCrumbsThisSacrifice = Decimal.fromString('1')
+export const defaultHighestCrumbsEver = Decimal.fromString('1')
