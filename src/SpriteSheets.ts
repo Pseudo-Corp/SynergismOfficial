8bb16ab42581551a6c2737a76956a14f03c790dc
Kevin Bunn <45328239+Pseudonian@users.noreply.github.com>
2026-03-13 20:58:50 -0400

    Improve SpriteSheet, add other Folders

--- src/SpriteSheets.ts
+++ src/SpriteSheets.ts
@@ -1,48 +1,59 @@
 import { DOMCacheGetOrSet } from "./Cache/DOM"
 
 export type iconSize = 32 | 64
-export interface SpriteSheetEntry {
-    // x and y here are such that (0,0) corresponds to top left image
-    // (1,0) is the next image to the right, etc
-    x: number,
-    y: number,
-    elementName: string
-}
 
 export interface SpriteSheet {
     name: string,
-    icons: SpriteSheetEntry[]
+    elementNames: string[]
     iconSize: iconSize // All icons in the sheet must be the same size.
+    rows: number
+    columns: number
 }
 
-// Add more sprite sheets here as implemented
-// ASSUMES THESE SHEETS EXIST ON ALL ICONSETS.
+// When you make new icons (or are converting old ones) make sure to add to this list
 export const spriteSheets: SpriteSheet[] = [
     {
         name: 'WowPasses',
-        icons: [
-            { x: 0, y: 0, elementName: 'seasonPass' },
-            { x: 1, y: 0, elementName: 'seasonPass2' },
-            { x: 2, y: 0, elementName: 'seasonPass3' },
-            { x: 3, y: 0, elementName: 'seasonPassY' },
-            { x: 4, y: 0, elementName: 'seasonPassZ' },
-            { x: 5, y: 0, elementName: 'seasonPassLost' },
-            { x: 6, y: 0, elementName: 'seasonPassInfinity' },
+        elementNames: [
+            'seasonPass',
+            'seasonPass2',
+            'seasonPass3',
+            'seasonPassY',
+            'seasonPassZ',
+            'seasonPassLost',
+            'seasonPassInfinity',
         ],
-        iconSize: 64
+        iconSize: 64,
+        rows: 1,
+        columns: 7
     }
 ]
 
 export const updateIconsFromSprites = (folderUsed: String) => {
     for (const sheet of spriteSheets) {
-        for (const icon of sheet.icons) {
-            const element = DOMCacheGetOrSet(icon.elementName)
+        let currRow = 0
+        let currCol = 0
+        for (const elementName of sheet.elementNames) {
+            const element = DOMCacheGetOrSet(elementName)
+            /* Why backgroundImage? If we use .src, the image is the entire sheet,
+             Condensed into something of size iconSize * iconSize.
+             In index.html, each of these elements has src img_transparent, so
+             we need to use the sprite as the background image, using only
+             the relevant portion of the sheet
+            */
             element.style.backgroundImage = `url('Pictures/${folderUsed}/Sprite Sheets/${sheet.name}.png')`
-            const backgroundX = -icon.x * sheet.iconSize
-            const backgroundY = -icon.y * sheet.iconSize
+            const backgroundX = -currCol * sheet.iconSize
+            const backgroundY = -currRow * sheet.iconSize
             element.style.backgroundPosition = `${backgroundX}px ${backgroundY}px`
             element.style.width = `${sheet.iconSize}px`
             element.style.height = `${sheet.iconSize}px`
+
+            // This structure means images are laid out top left to bottom right order
+            currCol++
+            if (currCol >= sheet.columns) {
+                currCol = 0
+                currRow++
+            }
         }
     }
 }
