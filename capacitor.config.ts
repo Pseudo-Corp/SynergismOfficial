5f0ab43eae29a425161f0d8c9ec18b801d2d38c4
Khafra <maitken033380023@gmail.com>
2026-04-17 12:11:11 -0400

    run 4 commands

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli'
+
+const config: CapacitorConfig = {
+  appId: 'cc.pseudocorp.synergism',
+  appName: 'Synergism',
+  webDir: 'dist'
+}
+
+export default config
