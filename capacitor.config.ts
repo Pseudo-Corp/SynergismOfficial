e8a8717977e294988c33e148e1a52367c05ec656
Matthew Aitken <maitken033380023@gmail.com>
2026-04-23 20:42:39 -0700

    login works

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,12 @@ import type { CapacitorConfig } from '@capacitor/cli'
 const config: CapacitorConfig = {
   appId: 'cc.pseudocorp.synergism',
   appName: 'Synergism',
-  webDir: 'dist'
+  webDir: 'dist',
+  plugins: {
+    CapacitorHttp: {
+      enabled: true
+    }
+  }
 }
 
 export default config
