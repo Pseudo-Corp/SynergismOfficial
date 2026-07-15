27108d41cbd2b94b1afc48d27553a694abdfda43
rus9384 <rus9384@yandex.ru>
2026-07-15 21:44:21 +0300

    Replaced regex with loop scanning

--- src/Plugins/NumberLocale.ts
+++ src/Plugins/NumberLocale.ts
@@ -5,9 +5,17 @@ import Decimal from 'break_infinity.js'
 export default {
   type: 'postProcessor',
   name: 'NumberLocale',
-  process: (value: string): string => value.includes('num:') ? value.replace(
-    // Regex replacement is faster than doing it in a loop
-    /num:\(\(([^e)]*(e?)[^)]*)\)\)/g, //num:1e6000 => 1e6,000; num:1000000 => 1,000,000; num:0.1% => 0.1%
-    (_, num, exp) => format(Decimal.fromString(num), 5, exp !== 'e', true)
-  ) : value
+  process: (value: string): string => {
+    let result = ''
+    let start = 0, index = 0
+    while ((index = value.indexOf('num:((', start)) !== -1) {
+      const end = value.indexOf('))', index)
+      const num = value.slice(index + 6, end)
+      result += value.slice(start, index)
+      result += format(Decimal.fromString(num), 5, !num.includes('e'), true)
+      start = end + 2
+    }
+    result += value.slice(start)
+    return result
+  }
 } satisfies PostProcessorModule
