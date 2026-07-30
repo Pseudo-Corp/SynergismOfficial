6587d2a6285902148b1b8f98f0f0e205b190761f
Matthew Aitken <maitken033380023@gmail.com>
2026-07-30 11:17:07 -0400

    update dsn
    uts

--- src/mobile/sentry.ts
+++ src/mobile/sentry.ts
@@ -5,7 +5,7 @@ import { version } from '../Config'
 
 export const initSentry = () => {
   Sentry.init({
-    dsn: 'https://ce116243-70e7-4203-9550-08cf4c852970@sentry.synergism.cc/1',
+    dsn: 'https://ce11624370e74203955008cf4c852970@sentry.synergism.cc/1',
     tracesSampleRate: 0, // Bugsink is error tracking only; it discards tracing data
     release: version,
     sendClientReports: false, // Errors only
