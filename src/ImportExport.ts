dc22b1aa017df1fab3da70e09b0fcc5da27eae2f
Matthew Aitken <maitken033380023@gmail.com>
2026-08-13 20:08:31 -0400

    fixup

--- src/ImportExport.ts
+++ src/ImportExport.ts
@@ -187,24 +187,31 @@ export const exportData = async (text: string, fileName: string) => {
   const toClipboard = getElementById<HTMLInputElement>('saveType').checked
 
   if (PLATFORM === 'mobile') {
-    const exportInfo = DOMCacheGetOrSet('exportinfo')
+    const { Capacitor } = await import('@capacitor/core')
+    const pluginsAvailable = toClipboard
+      ? Capacitor.isPluginAvailable('Clipboard')
+      : Capacitor.isPluginAvailable('Filesystem') && Capacitor.isPluginAvailable('Share')
 
-    try {
-      const { exportMobileData } = await import('./mobile/export')
-      const exported = await exportMobileData(text, fileName, toClipboard)
-      exportInfo.textContent = exported
-        ? i18next.t(toClipboard ? 'importexport.copiedSave' : 'importexport.exportedFile')
-        : ''
-    } catch (error) {
-      console.error('Failed to export save on mobile', error)
-      exportInfo.textContent = i18next.t('importexport.exportFailed')
-      await Alert(i18next.t(
-        toClipboard ? 'importexport.unableCopySave' : 'importexport.unableExportSave'
-      ))
+    if (pluginsAvailable) {
+      const exportInfo = DOMCacheGetOrSet('exportinfo')
+
+      try {
+        const { exportMobileData } = await import('./mobile/export')
+        const exported = await exportMobileData(text, fileName, toClipboard)
+        exportInfo.textContent = exported
+          ? i18next.t(toClipboard ? 'importexport.copiedSave' : 'importexport.exportedFile')
+          : ''
+      } catch (error) {
+        console.error('Failed to export save on mobile', error)
+        exportInfo.textContent = i18next.t('importexport.exportFailed')
+        await Alert(i18next.t(
+          toClipboard ? 'importexport.unableCopySave' : 'importexport.unableExportSave'
+        ))
+      }
+
+      setTimeout(() => (exportInfo.textContent = ''), 15_000)
+      return
     }
-
-    setTimeout(() => (exportInfo.textContent = ''), 15_000)
-    return
   }
 
   if (toClipboard) {
