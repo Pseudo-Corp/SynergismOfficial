9590b688e8609beb3c846d17af5d0b0524fa2076
Kevin Bunn <45328239+Pseudonian@users.noreply.github.com>
2026-08-14 15:15:58 -0400

    Purple Fusion Implementation

--- src/UpdateVisuals.ts
+++ src/UpdateVisuals.ts
@@ -2112,7 +2112,7 @@ export const visualUpdatePurple = () => {
   if (G.currentTab !== Tabs.Singularity) {
     return
   }
-
+  
   const capacity = Math.max(0, calculatePurpleReactantCapacity())
   const ambrosiaBarPoints = player.purpleReactor.storedAmbrosiaBarPoints
   const redAmbrosiaBarPoints = player.purpleReactor.storedRedAmbrosiaBarPoints
