b91da7997bbcdec4eb9950247882cc8b401d8e20
Kevin Bunn <45328239+Pseudonian@users.noreply.github.com>
2026-08-22 18:47:08 -0400

    ready for a test?

--- src/Config.ts
+++ src/Config.ts
@@ -5,7 +5,7 @@ export const isSynergismCC = location.hostname === 'synergism.cc'
 /**
  * If true, the version is marked as a testing version.
  */
-export const testing = false
+export const testing = true
 export const lastUpdated = new Date('##LAST_UPDATED##')
 
 export const ticksPerSecond = PLATFORM === 'mobile' ? 40 : 200
