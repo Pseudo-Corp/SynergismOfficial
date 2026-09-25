ac5895e8047f3417e1e1caf4f63bba3dd33ee8b6
Kevin Bunn <45328239+Pseudonian@users.noreply.github.com>
2026-09-25 17:03:15 -0400

    vers

--- src/Config.ts
+++ src/Config.ts
@@ -1,4 +1,4 @@
-export const version = '4.3.0 September 16, 2026: The Purple'
+export const version = '4.3.1 September 25, 2026: The Purple'
 
 export const isSynergismCC = location.hostname === 'synergism.cc'
 
